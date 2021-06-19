9ebc42d7e7cdc816a0ec8b58240b845362570668
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-06-19 16:08:02 +0530

    Update test_Receiver.cpp

--- test_Receiver.cpp
+++ test_Receiver.cpp
@@ -6,7 +6,7 @@
 #include "string.h"
 
 
-TEST_CASE("Test case to test the Maximum Value with less Value")
+TEST_CASE("Test case to test the Maximum Value with lesser Value")
 {
   float current_value = 10.1;
   float prev_maxi = 20.2;
@@ -14,3 +14,56 @@ TEST_CASE("Test case to test the Maximum Value with less Value")
   REQUIRE(Calc_Maxi(current_value, prev_maxi) == max_value);
 }
 
+TEST_CASE("Test case to test the Maximum Value with greater Value")
+{
+  float current_value = 100.8;
+  float prev_maxi = 2;
+  float max_value = 100.8;
+  REQUIRE(Calc_Maxi(current_value, prev_maxi) == max_value);
+}
+
+TEST_CASE("Test case to test the Maximum Value with Negative Value")
+{
+  float current_value = -1.8;
+  float prev_maxi = -29;
+  float max_value = -1.8;
+  REQUIRE(Calc_Maxi(current_value, prev_maxi) == max_value);
+}
+
+TEST_CASE("Test case to test the Minimum Value with lesser Value")
+{
+  float current_value = 9.8;
+  float prev_mini = 11.8;
+  float mini_value = 9.8;
+  REQUIRE(Calc_Mini(current_value, prev_mini) == mini_value);
+}
+
+TEST_CASE("Test case to test the Minimum Value with greater Value")
+{
+  float current_value = 187.5;
+  float prev_mini = 181.8;
+  float mini_value = 181.8;
+  REQUIRE(Calc_Mini(current_value, prev_mini) == mini_value);
+}
+
+TEST_CASE("Test case to test the Minimum Value with negative Value")
+{
+  float current_value = -0.5;
+  float prev_mini = -1.8;
+  float mini_value = -0.5;
+  REQUIRE(Calc_Mini(current_value, prev_mini) == mini_value);
+}
+
+TEST_CASE("Test case to test the Average Value")
+{
+  float arr[] = {10.3,10.1};
+  float average = 10.2;
+  REQUIRE(Calc_Average(arr, 2) == average);
+}
+
+TEST_CASE("Test case to test the null values")
+{
+  float arr[] = NULL;
+  float average = 0;
+  REQUIRE(Calc_Average(arr, 2) == average);
+}
