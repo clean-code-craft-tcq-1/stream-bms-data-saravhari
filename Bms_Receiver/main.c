92fd8ecbfbf48b5a1ac80599113e839080393c8b
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-06-19 15:47:25 +0530

    Create main.c

---
+++ Bms_Receiver/main.c
@@ -0,0 +1,20 @@
+/* File Details ***********************************************************************************************
+* File Name   :	main.c
+* Author      : Arul Kumar Sundaramoorthy
+* Description : Receives the BMS Data & perform calculations on received values
+* Functions   : main
+* *********************************************************************************************************** */
+
+/* **************************************** Header Files **************************************************** */
+#include "Receiver_DataCalc.h"
+
+/* Function Details *******************************************************************************************
+* Function Name : main
+* Description   : main function to receive the Data
+* Arguments     : -
+* Returns       : 0
+* *********************************************************************************************************** */
+int main() {
+    
+    return 0;
+}
