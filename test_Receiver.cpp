4b453d7b6c8d598de912dd71da7f06d9ecdb39c5
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-06-19 14:39:41 +0530

    Create test_Receiver.cpp

---
+++ test_Receiver.cpp
@@ -0,0 +1,13 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include "BMS_Receiver/Receiver_DataCalc.h"
+#include "stddef.h"
+#include "string.h"
+
+
+TEST_CASE("Test case to test the Maximum Value with less Value")
+{
+  REQUIRE(Calc_Maxi(10.1, 20.1)== 20.1);
+}
+
