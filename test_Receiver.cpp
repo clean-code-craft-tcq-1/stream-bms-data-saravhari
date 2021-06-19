a958150d962892ddde7c9905cb71c26bbb52f7c0
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-06-19 14:43:45 +0530

    Update test_Receiver.cpp

--- test_Receiver.cpp
+++ test_Receiver.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
-#include "BMS_Receiver/Receiver_DataCalc.h"
+#include "Bms_Receiver/Receiver_DataCalc.h"
 #include "stddef.h"
 #include "string.h"
 
