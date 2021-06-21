41649a887e8112067e930a6c67ef994fb00258dd
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-06-22 03:25:56 +0530

    Update Receiver_RxData.h

--- Bms_Receiver/Receiver_RxData.h
+++ Bms_Receiver/Receiver_RxData.h
@@ -16,4 +16,8 @@ void receive_data(void);
 void analyse_data(float temperature, float soc, float chargerate);
 void decode_data(char *Copy_buffer, float *temperature, float *soc, float *chargerate);
 
+#if(TEST_ON == TRUE)
+void test_Scanf(char *rv_data);
+#endif
+
 #endif
