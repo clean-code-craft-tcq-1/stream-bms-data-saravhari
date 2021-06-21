b0181aabb4b039c093d07b4432d0272556d666a4
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-06-21 15:08:44 +0530

    Update main.c

--- Bms_Receiver/main.c
+++ Bms_Receiver/main.c
@@ -12,6 +12,20 @@
 #include "stdlib.h"
 #include "string.h"
 #include "ctype.h"
+#include "time.h"
+
+void delay(int seconds)
+{
+  // Converting time into milli_seconds
+  int milli_seconds = 1000 * seconds;
+
+  // Storing start time
+  clock_t start_time = clock();
+
+  // looping till required time is not achieved
+  // which is begin time + wait time in milliseconds 
+  while (clock() < start_time + milli_seconds);
+}
 
 /* Function Details *******************************************************************************************
 * Function Name : main
@@ -20,8 +34,12 @@
 * Returns       : 0
 * *********************************************************************************************************** */
 int main() {
+    for(i=0;i<20;i++)
+    {
     char rv_data[1000];
     scanf("%s", rv_data);
-    printf("\n\n\nReceived Data* %s\n\n",rv_data);
+    printf("\n\n\n%d-Received Data* %s\n\n",i,rv_data);
+        delay(2);
+    }
     return 0;
 }
