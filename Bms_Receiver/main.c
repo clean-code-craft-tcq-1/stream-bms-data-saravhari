8da60f07ed2b0a4756095222f3a2f8cf1695c38b
Arul Kumar Sundaramoorthy <arulkumarmoorthy@gmail.com>
2021-06-21 17:00:58 +0530

    Update main.c

--- Bms_Receiver/main.c
+++ Bms_Receiver/main.c
@@ -39,10 +39,11 @@ void delay(int seconds)
 int main() {
     for(int i=0;i<20;i++)
     {
+      delay(2);
     char rv_data[1000];
     scanf("%s", rv_data);
     printf("\n\n\n%d-Received Data* %s\n\n",i,rv_data);
-        delay(2);
+        
       for(int j=0;j<=10000;j++)
       {
         for(int k=0;k<=10000;k++)
